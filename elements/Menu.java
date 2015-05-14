ada48bad331219fba3bb049d9a7c61527b6d3362
IceColdCode <IceCodeDev@gmail.com>
2015-05-14 11:40:57 -0600

    Update Menu.java

--- elements/Menu.java
+++ elements/Menu.java
@@ -1,1 +1,27 @@
+package JSprite.elements;
 
+import java.awt.*;
+import java.awt.event.*
+import javax.swing.*
+import javax.imageio.ImageIO;
+
+public class Menu extends JPanel {
+  
+  String[] menuOptions;
+  BufferedImage background;
+  
+  // constructor time!
+  public Menu(String backgroundFileName, int height, int width, String[] optionsToUse) {
+    
+    int i;
+    while(i != null) {
+      
+      menuOptions[i] += optionsToUse[i];
+      
+    }
+    
+    background = new BufferedImage(backgroundFileName)
+    
+  }
+  
+}
